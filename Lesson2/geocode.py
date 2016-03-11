0368439eb83d590907bc2ad490e44480a2438790
ntromboukis <ntromboukis@gmail.com>
2016-03-10 23:50:06 -0500

    Fix: Removed API Key Replaced API Key with placeholder text

--- Lesson2/geocode.py
+++ Lesson2/geocode.py
@@ -2,11 +2,12 @@ import httplib2
 import json
 
 def getGeocodeLocation(inputString):
-	# google_api_key = "PASTE_YOUR_STRING_HERE"
-	google_api_key = "AIzaSyAqXiaJq9rE_O1eMBPZCkWVVsnboXzLoME"
+	google_api_key = "PASTE_YOUR_KEY_HERE"
 	locationString = inputString.replace(" ", "+")
 	url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key%s'% (locationString, google_api_key))
 	h = httplib2.Http()
 	response, content = h.request(url, 'GET')
 	result = json.loads(content)
-	return result
+	latitude = result['results'][0]['geometry']['location']['lat']
+	longitude = result['results'][0]['geometry']['location']['lng']
+	return (str(latitude) + "," + str(longitude))

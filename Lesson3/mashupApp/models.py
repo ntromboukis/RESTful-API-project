787d6bb11223f7a7fc0f7e984a8312a781fb9aec
ntromboukis <ntromboukis@gmail.com>
2016-03-13 11:17:04 -0400

    Update: Changed name of table and its columns

--- Lesson3/mashupApp/models.py
+++ Lesson3/mashupApp/models.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -6,23 +5,22 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Restaurant(Base):
-    __tablename__ = 'restaurant'
+    __tablename__ = 'restaurants'
 
-
-    name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
-    location = Column(String(250))
-    photoUrl = Column(String(250))
+    restaurant_name = Column(String(80), nullable = False)
+    restaurant_address = Column(String)
+    restaurant_image = Column(String)
     #Add add a decorator property to serialize data from the database
     @property
     def serialize(self):
         return {
-        'name'          : self.name,
-        'id'            : self.id,
-        'location'      : self.location,
-        'photo'         : self.photoUrl
+        'restaurant_name'     : self.restaurant_name,
+        'id'                  : self.id,
+        'restaurant_address' : self.restaurant_address,
+        'restaurant_image'    : self.restaurant_image
         }
 
 
-engine = create_engine('sqlite:///restaurant.db')
+engine = create_engine('sqlite:///restaurants.db')
 Base.metadata.create_all(engine)

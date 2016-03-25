1a409d47c0cc3c55f794b3aaa596e43c7d827c85
ntromboukis <ntromboukis@gmail.com>
2016-03-24 20:27:16 -0400

    feat: Add /token route

--- Lesson4/regal-tree-foods/views.py
+++ Lesson4/regal-tree-foods/views.py
@@ -16,15 +16,14 @@ session = DBSession()
 app = Flask(__name__)
 
 
-
-
 #ADD @auth.verify_password decorator here
 
-
 #add /token route here to get a token for a user with login credentials
-
-
-
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token', token.decode('ascii')})
 
 @app.route('/users', methods = ['POST'])
 def new_user():
